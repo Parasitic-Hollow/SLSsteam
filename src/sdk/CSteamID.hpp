df8bf2434ab7634ef4e62638d1903720c003af56
Ace SLS <acesls@protonmail.com>
2026-01-24 22:34:34 +0100

    feat(CSteamId): Add missing fields

--- src/sdk/CSteamID.hpp
+++ src/sdk/CSteamID.hpp
@@ -7,7 +7,8 @@
 class CSteamId
 {
 public:
-	uint32_t steamId; //0x4
-	uint8_t universe; //0x5
-	char _pad_0x5[0x3]; //0x8
-};
+	uint32_t steamId; //0x0
+	uint16_t type; //0x4 - Might be universe aswell
+	uint8_t instance; //0x6
+	uint8_t universe; //0x7 Might be type aswell
+}; //0x8
